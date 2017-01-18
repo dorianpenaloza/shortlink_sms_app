90667bf814f9dcfadb2a17e7d23bdbd17c354af8
dorianpenaloza <dorianpenaloza@yahoo.com>
2017-01-17 21:53:18 -0600

    added shortlink function

---
+++ shortlink_sms_app/config/initializers/bitly.rb
@@ -0,0 +1,5 @@
+Bitly.configure do |config|
+  config.api_version = 3
+  config.login = "dorianpenaloza"
+  config.api_key = "R_51acf6a049c34d87b07d93839f24caed"
+end
--- shortlink_sms_app/config/routes.rb
+++ shortlink_sms_app/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  resources :posts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   get 'urls/new'
+#
+   get 'urls/create'
+#
+   resources :posts
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
+#
+# BitlyExample::Application.routes.draw do
+  resources :urls, only: [:new, :create]
+  root to: 'urls#new'
 end
